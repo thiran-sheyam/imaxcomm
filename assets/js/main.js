4c61cda4ea94d1efd7c068c290841e06fb28477a
sheyam <sheyam@thirantech.com>
2019-05-15 21:36:46 +0530

    commss
    commss

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,5 +108,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 73 * 17 * 54 * 60 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 72 * 17 * 54 * 60 * 1000);
 initializeClock('clockdiv', deadline);
